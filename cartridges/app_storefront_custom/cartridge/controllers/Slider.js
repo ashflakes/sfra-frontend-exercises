63cc817e5aec8e3da12a1d6d69bb4eeba22c585d
George Cristi <ashflakes@gmail.com>
2022-10-24 19:53:28 +0300

    exercise-8 sovled
    need revisiting for 1 console error

--- cartridges/app_storefront_custom/cartridge/controllers/Slider.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Slider.js
@@ -3,9 +3,13 @@
 var server = require('server');
 
 server.get('Render', function (req, res, next) {
-    
     res.render("carousel/carousel");
     return next();
 });
+
+server.get('RenderProductSlider', function(req, res, next) {
+    res.render('slider/tenProductSlider');
+    return next();
+})
 
 module.exports = server.exports();
