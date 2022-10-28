9f31a6c1ee408c34eaa504b5a82465456070e56e
George Cristi <ashflakes@gmail.com>
2022-10-28 08:27:07 +0300

    exercise-13-solved

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -6,3 +6,10 @@ $(document).ready(function () {
     processInclude(require('./components/test'));
 });
 
+$("#comment").keyup(function () {
+    var charLength = $(this).val().length;
+    var charLimit = $(this).attr("size");
+
+    var charLeft = charLimit - charLength
+    $("#remaining-chars").html("You have " +  charLeft + " characters left out of " + charLimit + "." );
+})
