0ff5bb8a241cb26dc1949cff8e18c7b27886be30
George Cristi <ashflakes@gmail.com>
2022-10-30 17:14:11 +0200

    exercise-2-final-only-css
    final version for exrercise 2, only css
    marked in isml comments the changes

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -5,23 +5,3 @@ $(document).ready(function () {
     processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
     processInclude(require('./components/test'));
 });
-
-
-
-
-
-//$('.promo-code-form').css('visibility', 'hidden');
-
-$('.handle-coupon').on('click', function (e) {
-    $('.handle-coupon').hide();
-    $('.promo-code-form').css('display','block');
-})
-
-
-// $('.promo-code-form').on('click', function (e) {
-//     e.preventDefault();
-//     $('.promo-code-form').toggle();
-// });
-
-
-
