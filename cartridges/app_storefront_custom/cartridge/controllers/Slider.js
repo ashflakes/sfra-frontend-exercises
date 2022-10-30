8cf0ba2b8a49c6f014dde87d0ede5a0c7680b938
George Cristi <ashflakes@gmail.com>
2022-10-30 22:41:25 +0200

    exercise6-solved

--- cartridges/app_storefront_custom/cartridge/controllers/Slider.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Slider.js
@@ -3,8 +3,29 @@
 var server = require('server');
 
 server.get('Render', function (req, res, next) {
-    
-    res.render("carousel/carousel");
+    var sliderURLs = {
+        slickbasicURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css",
+        slickthemeURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css",
+        jqueryslickURL: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js",
+        slickURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
+    }
+
+    var imageURLs = [];
+
+    function getImageURLs(id) {
+        var baseURL = `https://source.unsplash.com/random/200x200?sig=${id}`;
+
+        return baseURL + id;
+    }
+
+    for(let i = 1; i < 9 ; i++) {
+        imageURLs.push(getImageURLs(i))
+    }
+
+    res.render("carousel/carousel", {
+        sliderURLs,
+        imageURLs
+    });
     return next();
 });
 
